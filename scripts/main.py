9b894a24f5e475eac68443a092ea8d7381cc4c0b
Cameron Arshadi <arshadic23@gmail.com>
2023-12-12 20:17:46 -0800

    feat: adds main script

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,4 @@
 import logging
-import tempfile
 from glob import glob
 import os
 import argparse
@@ -7,7 +6,7 @@ from pathlib import Path
 
 from aind_morphology_utils import coordinate_mapping
 from aind_morphology_utils.ccf_annotation import CCFMorphologyMapper
-from aind_morphology_utils.coordinate_mapping import HDF5Transform
+from aind_morphology_utils.coordinate_mapping import OMEZarrTransform
 from aind_morphology_utils.utils import read_swc
 from aind_morphology_utils.writers import MouseLightJsonWriter
 from merge_tracings import merge_swcs_in_folder
@@ -19,19 +18,16 @@ _LOGGER = logging.getLogger(__name__)
 
 def _parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--reg-dir', type=str,
-                        default=r"C:\Users\cameron.arshadi\Documents\registrations\651324\registration")
-    parser.add_argument('--slicer-transform', type=str, default=r"C:\Users\cameron.arshadi\Documents\registrations\651324\registration\Transform.651324.h5")
-    parser.add_argument('--swc-dir', type=str,
-                        default=r'C:\Users\cameron.arshadi\Documents\registrations\651324\exaSPIM_651324_2023-03-06_15-13-25\Complete')
-    parser.add_argument('--image-path', type=str,
-                        default="s3://aind-open-data/exaSPIM_651324_2023-03-06_15-13-25_fusion_2023-03-28_17-06-00/fused.zarr")
-    parser.add_argument('--output-dir', type=str, default=r'..\results\651324-ccf_coords')
-    parser.add_argument('--output-json', default=True, action='store_true')
+    parser.add_argument('--reg-dir', type=str)
+    parser.add_argument('--slicer-transform', type=str)
+    parser.add_argument('--swc-dir', type=str)
+    parser.add_argument('--image-path', type=str)
+    parser.add_argument('--output-dir', type=str)
+    parser.add_argument('--output-json', default=False, action='store_true')
     parser.add_argument('--transform-res', type=float, nargs='+', default=[25.0, 25.0, 25.0])
     parser.add_argument('--input-res', type=float, nargs='+', default=[0.421875, 0.421875, 0.5625])
     parser.add_argument('--swc-scale', type=float, nargs='+', default=[0.748, 0.748, 1])
-    parser.add_argument('--flip-axes', type=int, nargs='+', default=[0])
+    parser.add_argument('--flip-axes', type=int, nargs='+', default=[])
     parser.add_argument('--input-scale', type=int, default=5)
     parser.add_argument('--affine-only', default=True, action='store_true')
     parser.add_argument('--log-level', type=str, default=logging.INFO)
@@ -78,37 +74,46 @@ def main() -> None:
 
     slicer_transform = None
     if slicer_transform_file is not None:
-        slicer_transform = HDF5Transform(slicer_transform_file)
+        slicer_transform = OMEZarrTransform(slicer_transform_file)
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        merge_swcs_in_folder(neuron_folder, temp_dir, ignore_list=['unique', 'base'])
+    all_swcs = glob(os.path.join(neuron_folder, '**', '*.swc'), recursive=True)
 
-        all_swcs = glob(os.path.join(temp_dir, '**', '*.swc'), recursive=True)
+    aligned_dir = os.path.join(output_folder, 'aligned')
+    os.makedirs(aligned_dir, exist_ok=True)
 
-        for swc_file in all_swcs:
+    for swc_file in all_swcs:
+        _LOGGER.info(f"processing {swc_file}")
+
+        transformed = read_swc(swc_file, add_offset=True)
+
+        if ants_transform is not None:
+            transformed = ants_transform.transform(transformed)
+        if slicer_transform is not None:
+            transformed = slicer_transform.transform(transformed)
+
+        # save using same directory structure as input folder, make sure all directories exist
+        transformed_path = os.path.join(aligned_dir, os.path.relpath(swc_file, neuron_folder))
+        os.makedirs(os.path.dirname(transformed_path), exist_ok=True)
+        transformed.save(transformed_path)
+
+    merged_dir = os.path.join(output_folder, 'merged')
+    os.makedirs(merged_dir, exist_ok=True)
+    merge_swcs_in_folder(aligned_dir, merged_dir, ignore_list=['unique', 'base'])
+
+    if args.output_json:
+        json_dir = os.path.join(output_folder, 'json')
+        os.makedirs(json_dir, exist_ok=True)
+        for swc_file in glob(os.path.join(merged_dir, '*.swc')):
             _LOGGER.info(f"processing {swc_file}")
-
             transformed = read_swc(swc_file, add_offset=True)
-
-            if ants_transform is not None:
-                transformed = ants_transform.transform(transformed)
-            if slicer_transform is not None:
-                transformed = slicer_transform.transform(transformed)
-
-            if args.output_json:
-                _LOGGER.info(f"writing {swc_file} as JSON")
-                # Map CCF regions and save as JSON
-                ccf_mapper = CCFMorphologyMapper(resolution=args.transform_res[0])
-                ccf_mapper.annotate_morphology(transformed)
-                writer = MouseLightJsonWriter(
-                    transformed,
-                    id_string=Path(swc_file).stem,
-                )
-                writer.write(os.path.join(args.output_dir, Path(swc_file).stem + '.json'))
-
-            else:
-                # Save as SWC
-                transformed.save(os.path.join(args.output_dir, Path(swc_file).name))
+            # Map CCF regions and save as JSON
+            ccf_mapper = CCFMorphologyMapper(resolution=args.transform_res[0])
+            ccf_mapper.annotate_morphology(transformed)
+            writer = MouseLightJsonWriter(
+                transformed,
+                id_string=Path(swc_file).stem,
+            )
+            writer.write(os.path.join(json_dir, Path(swc_file).stem + '.json'))
 
 
 if __name__ == "__main__":
